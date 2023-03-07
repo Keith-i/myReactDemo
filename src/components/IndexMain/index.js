6a7a85ad54c7cb5fa16122ce3ede5fdb1dec0400
Keith <754781293@qq.com>
2023-03-07 18:51:34 +0800

    提交

--- src/components/IndexMain/index.js
+++ src/components/IndexMain/index.js
@@ -1,9 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Tabbar } from "react-vant";
+import { useState } from "react";
+import { FriendsO, HomeO, Search } from "@react-vant/icons";
+
 let IndexMain = () => {
+  const [name, setName] = useState("/home");
+  const navigate = useNavigate();
+  const goLink = (v) => {
+    navigate(v, {
+      replace: true,
+    });
+    setName(v);
+  };
   return (
-    <div className="main">
-      <Outlet />
-    </div>
+    <>
+      <div className="main">
+        <Outlet />
+      </div>
+      <div className="demo-tabbar">
+        <Tabbar value={name} onChange={(v) => goLink(v)}>
+          <Tabbar.Item name="/home" icon={<HomeO />}>
+            首页
+          </Tabbar.Item>
+          <Tabbar.Item name="/list" icon={<Search />}>
+            列表
+          </Tabbar.Item>
+          <Tabbar.Item name="/my" icon={<FriendsO />}>
+            我的
+          </Tabbar.Item>
+        </Tabbar>
+      </div>
+    </>
   );
 };
 
