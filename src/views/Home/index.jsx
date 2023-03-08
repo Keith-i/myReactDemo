4ea32556471c11e1d335b016b6fe7c421d908b31
Keith <754781293@qq.com>
2023-03-08 11:36:59 +0800

    跳转

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,8 +1,66 @@
 import "./style.scss";
-import { Button } from "react-vant";
-let Home = () => {
+import { Button, Swiper, Toast } from "react-vant";
+
+// 轮播内容
+let MySwiperItem = () => {
+  const colors = ["#ace0ff", "#bcffbd", "#e4fabd", "#ffcfac"];
+  let items = colors.map((color, index) => (
+    <Swiper.Item key={color}>
+      <div
+        className="itembox"
+        style={{ background: color }}
+        onClick={() => {
+          Toast.info(`你点击了卡片${index + 1}`);
+        }}
+      ></div>
+    </Swiper.Item>
+  ));
+  return items;
+};
+
+// 卡种列表
+let CardBox = (props) => {
   return (
     <>
+      <div>选择卡种回收</div>
+      <div className="groupbox">
+        <div className="gridbox">
+          {props.list.map((item) => (
+            <div className="vanitem" key={item.id}>
+              <img
+                src={item.img_url}
+                alt={item.img_url}
+              />
+              <div className="vanname">{item.name}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+// 主要内容
+let Home = () => {
+  const cardList = [
+    {
+      id: 1,
+      name: "万里通",
+      img_url:
+        "http://mtest.mf178.cn/uploads/mf_card/card_kind_config/card_kind_config1611905230.png",
+    },
+    {
+      id: 2,
+      name: '联通话费卡',
+      img_url: 'http://mtest.mf178.cn/uploads/mf_card/card_kind_config/card_kind_config1611905119.png'
+    },
+  ];
+  return (
+    <>
+      <div className="banner">
+        <Swiper autoplay={3000}>{MySwiperItem()}</Swiper>
+      </div>
+      <CardBox list={cardList} />
       <div className="title">这是首页</div>
       <Button>我是内容新</Button>
     </>
