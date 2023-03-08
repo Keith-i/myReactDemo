6ee0b1120585a80cd21896117ccd39433b3ae472
Keith <754781293@qq.com>
2023-03-08 16:20:40 +0800

    提交

--- src/views/My/index.jsx
+++ src/views/My/index.jsx
@@ -1,12 +1,32 @@
 // import "./style.scss";
 import styles from "./style.module.scss";
 import React from "react";
-console.log(styles)
+console.log(styles);
 // 顶部内容
 const Topcom = () => {
   return (
-    <div className="headbox">
-      <div className={styles.headtit}>测试内容<span className={styles.myt}>ccc</span></div>
+    <div className={styles.headbox}>
+      <img
+        src="http://ktest.mf178.cn/themes/vue/img/test.96a573c9.png"
+        alt="http://ktest.mf178.cn/themes/vue/img/test.96a573c9.png"
+      />
+      <div className={styles.txtbox}>
+        <div className={styles.name}>小明</div>
+        <div className={styles.userid}>用户ID：00001</div>
+      </div>
+    </div>
+  );
+};
+
+// 钱包部分
+const Wallet = () => {
+  return (
+    <div className={styles.pricebox}>
+      <div className={styles.pricecenter}>
+        <div className={styles.tit}>未提现金额</div>
+        <div className={styles.pricemm}>1000000元</div>
+        <div className={styles.btn}>快速提现</div>
+      </div>
     </div>
   );
 };
@@ -16,7 +36,7 @@ class My extends React.Component {
     return (
       <>
         <Topcom />
-        <div>我的</div>
+        <Wallet />
       </>
     );
   }
