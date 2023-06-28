890195786f5dc7d047657088c50463313781a248
Keith <754781293@qq.com>
2023-06-28 15:09:20 +0800

    提交

--- src/views/ColorBoard/index.jsx
+++ src/views/ColorBoard/index.jsx
@@ -1,10 +1,29 @@
-import "./style.scss";
+import React, { useState, useEffect } from "react";
+import { Uploader } from "react-vant";
 
-let ColorBoard = () => {
+const ColorBoard = () => {
+  const [uploadData, setUploadData] = useState([]);
+
+  const onChangeFn = (file) => {
+    console.log(file, "file");
+    setUploadData(file);
+  };
+
+  useEffect(() => {
+    console.log(uploadData, "uploadData");
+  }, [uploadData]);
+
   return (
-    <>
-      <div>颜色板</div>
-    </>
+    <div>
+      <Uploader accept="image/*" value={uploadData} onChange={onChangeFn} />
+      <div
+        onClick={() => {
+          console.log(uploadData, "uploadData");
+        }}
+      >
+        测试
+      </div>
+    </div>
   );
 };
 
