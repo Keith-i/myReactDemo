5ef240479c63595e980193f771cdbb36c0f6427f
Keith <754781293@qq.com>
2023-06-29 15:52:35 +0800

    提交

--- src/views/JsonFormatting/index.jsx
+++ src/views/JsonFormatting/index.jsx
@@ -1,12 +1,22 @@
-// JSON格式化工具
-import "./style.scss";
+import React from "react";
+import ReactJson from "react-json-view";
 
-let JsonFormatting = () => {
+const JsonFormatting = () => {
+  const jsonData = {
+    name: "John Doe",
+    age: 25,
+    address: {
+      street: "123 Main St",
+      city: "New York",
+      country: "USA",
+    },
+  };
+
   return (
-    <>
-      <div>工具</div>
-      <div>JSON</div>
-    </>
+    <div>
+      <h2>JSON 数据格式化</h2>
+      <ReactJson src={jsonData} theme="rjv-default" />
+    </div>
   );
 };
 
